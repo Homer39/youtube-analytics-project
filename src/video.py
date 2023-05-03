c2b9fde5988901034560e68c63fbda9ea49bc181
Vlad <sapozhnikov.061098@mail.ru>
2023-05-03 14:47:30 +0300

    Исправлены классы Video и PLVideo

--- src/video.py
+++ src/video.py
@@ -5,13 +5,14 @@ import os
 class Video:
     """Класс для ютуб-канала"""
     api_key = os.getenv('API_KEY_YOUTUBE')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def __init__(self, video_id, video_title, view_count, like_count):
+    def __init__(self, video_id):
         self.__video_id = video_id
-        self.__video_title = video_title
-        self.__view_count = int(view_count)
-        self.__like_count = int(like_count)
+        response_video = Video.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                           id=self.__video_id).execute()
+        self.__video_title: str = response_video['items'][0]['snippet']['title']
+        self.__view_count: int = response_video['items'][0]['statistics']['viewCount']
+        self.__like_count: int = response_video['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         return self.__video_title
@@ -37,41 +38,31 @@ class Video:
         return f'https://www.youtube.com/watch?v={self.__video_id}'
 
     @classmethod
-    def get_service(cls, video_id):
-        response_video = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                     id=video_id
-                                                     ).execute()
-        for data in response_video['items']:
-            return cls(data['id'],
-                       data['snippet']['title'],
-                       data['statistics']['viewCount'],
-                       data['statistics']['likeCount'])
+    def get_service(cls):
+        youtube = build('youtube', 'v3', developerKey=Video.api_key)
+        return youtube
 
 
 class PLVideo(Video):
 
-    def __init__(self, video_id, video_title, view_count, like_count, playlist_id):
-        super().__init__(video_id, video_title, view_count, like_count)
+    def __init__(self, video_id, playlist_id):
+        response_video = PLVideo.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=video_id
+                                                             ).execute()
+        super().__init__(video_id)
+        self.__video_title = response_video['items'][0]['snippet']['title'],
+        self.__view_count: int = response_video['items'][0]['statistics']['viewCount']
+        self.__like_count: int = response_video['items'][0]['statistics']['likeCount']
         self.__playlist_id = playlist_id
 
     @property
     def playlist_id(self):
         return self.__playlist_id
 
-    @classmethod
-    def get_service(cls, video_id, playlist_id):
-        response_video = PLVideo.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                       id=video_id
-                                                       ).execute()
-        for data in response_video['items']:
-            return cls(data['id'],
-                       data['snippet']['title'],
-                       data['statistics']['viewCount'],
-                       data['statistics']['likeCount'],
-                       playlist_id)
 
-
-video1 = Video.get_service('9lO06Zxhu88')
-video2 = PLVideo.get_service('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
-print(str(video1))
-print(str(video2))
+if __name__ == '__main__':
+    # Создаем два экземпляра класса
+    video1 = Video('9lO06Zxhu88')
+    video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
+    assert str(video1) == 'Как устроена IT-столица мира / Russian Silicon Valley (English subs)'
+    assert str(video2) == 'Пушкин: наше все?'
